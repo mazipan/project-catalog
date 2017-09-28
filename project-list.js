1bb1beac274f40fafb62609a3ca0c977770db99e
Irfan Maulana <irfan.maulana@gdn-commerce.com>
2017-09-28 18:54:16 +0700

    add js and json file

---
+++ project-list.js
@@ -0,0 +1,145 @@
+export default {
+    name: 'project-list',
+    projects: [{
+            'name': 'ng2-simplert',
+            'url': 'https://github.com/mazipan/ng2-simplert',
+            'desc': 'Angular 2 Simple Alert Component (SweetAlert Inspired)'
+        },
+        {
+            'name': 'ng2-starwars',
+            'url': 'https://github.com/mazipan/ng2-starwars',
+            'desc': 'Star Wars API Implementation in Angular2'
+        },
+        {
+            'name': 'angularjs-basic-tutorial',
+            'url': 'https://github.com/mazipan/angularjs-basic-tutorial',
+            'desc': 'Angularjs v.1.x Basic Tutorial'
+        },
+        {
+            'name': 'react-simplert',
+            'url': 'https://github.com/mazipan/react-simplert',
+            'desc': 'ReactJS Simple Alert Component (SweetAlert Inspired)'
+
+        },
+        {
+            'name': 'Vue2-Simplert',
+            'url': 'https://github.com/mazipan/vue2-simplert',
+            'desc': 'Vue 2 Simple Alert Component (SweetAlert Inspired)'
+        },
+        {
+            'name': 'Vue2-Simplert-Plugin',
+            'url': 'https://github.com/mazipan/vue2-simplert-plugin',
+            'desc': 'Vue2-Simplert as Vue.js Plugins'
+        },
+        {
+            'name': 'vue-currency-filter',
+            'url': 'https://github.com/mazipan/vue-currency-filter',
+            'desc': 'Lightweight and Customizeable Vue 2 Currency Filter'
+        },
+        {
+            'name': 'vue-select-image',
+            'url': 'https://github.com/mazipan/vue-select-image',
+            'desc': 'Vue 2 Component for selecting image from list'
+        },
+        {
+            'name': 'vue-complexify',
+            'url': 'https://github.com/mazipan/vue-complexify',
+            'desc': 'Vuejs porting library from jquery.complexify.js'
+        },
+        {
+            'name': 'chucknorris',
+            'url': 'https://github.com/mazipan/chucknorris',
+            'desc': 'Chuck Norris Jokes built with VueJS + api.chucknorris.io'
+        },
+        {
+            'name': 'explore-github',
+            'url': 'https://github.com/mazipan/explore-github',
+            'desc': 'VueJS 2 Github Explorer Using API v3'
+        },
+        {
+            'name': 'FireJak',
+            'url': 'https://github.com/mazipan/FireJak',
+            'desc': 'Aplikasi Daftar Pos Pemadam Kebakaran di Jakarta'
+        },
+        {
+            'name': 'RumahSakitJakarta',
+            'url': 'https://github.com/mazipan/RumahSakitJakarta',
+            'desc': 'Daftar Rumah Sakit Umum, Khusus dan Puskesmas di Jakarta'
+        },
+        {
+            'name': 'ExtJS-NetworkDashboard',
+            'url': 'https://github.com/mazipan/ExtJS-NetworkDashboard',
+            'desc': 'ExtJS Professional Dashboard'
+        },
+        {
+            'name': 'ExtJS-TNMD',
+            'url': 'https://github.com/mazipan/ExtJS-TNMD',
+            'desc': 'TNMD - Through Network Monitoring Dashboard'
+        },
+        {
+            'name': 'nodejs-simple-contact-webapp',
+            'url': 'https://github.com/mazipan/nodejs-simple-contact-webapp',
+            'desc': 'Contact WebApp with ExpressJS - CRUD and Docs'
+        },
+        {
+            'name': 'SampleSPA-SwapiApi',
+            'url': 'https://github.com/mazipan/SampleSPA-SwapiApi',
+            'desc': 'Sample for creating SPA Page Using Swapi API'
+        },
+        {
+            'name': 'sample-swagger-for-nodejs',
+            'url': 'https://github.com/mazipan/sample-swagger-for-nodejs',
+            'desc': 'Sample of using swagger.io with Nodejs'
+        },
+        {
+            'name': 'nodejs-simple-restfull-with-express',
+            'url': 'https://github.com/mazipan/nodejs-simple-restfull-with-express',
+            'desc': 'ExpressJS Rest API Sample'
+        },
+        {
+            'name': 'nodejs-simple-restfull-test-with-mocha',
+            'url': 'https://github.com/mazipan/nodejs-simple-restfull-test-with-mocha',
+            'desc': 'Sample of Restful API Unit Test using Mocha'
+        },
+        {
+            'name': 'bem-kit',
+            'url': 'https://github.com/mazipan/bem-kit',
+            'desc': 'CSS UI Kit with BEM + SASS'
+        },
+        {
+            'name': 'bulma-admin-dashboard-template',
+            'url': 'https://github.com/mazipan/bulma-admin-dashboard-template',
+            'desc': 'Free Admin Dashboard Template Build with Bulma.io'
+        },
+        {
+            'name': 'lightweight-admin-template',
+            'url': 'https://github.com/mazipan/lightweight-admin-template',
+            'desc': 'Lightweight admin template for your web application'
+        },
+        {
+            'name': 'JavaSwing-SimpleApplicationDesktopTemplate',
+            'url': 'https://github.com/mazipan/JavaSwing-SimpleApplicationDesktopTemplate',
+            'desc': 'Basic Template for Desktop Application Window with Java Swing'
+        },
+        {
+            'name': 'Java-SwingCustomComponent',
+            'url': 'https://github.com/mazipan/Java-SwingCustomComponent',
+            'desc': 'Advance Custom Component for Java Swing'
+        },
+        {
+            'name': 'Java-PerpustakaanOnline',
+            'url': 'https://github.com/mazipan/Java-PerpustakaanOnline',
+            'desc': 'Sample of JavaEE using servlet'
+        },
+        {
+            'name': 'learn-php-oop-like-java',
+            'url': 'https://github.com/mazipan/learn-php-oop-like-java',
+            'desc': 'Sample of Object Oriented Programming in PHP Language that influenced from Java'
+        },
+        {
+            'name': 'bliblidotcom-chrome-extensions-unofficial',
+            'url': 'https://github.com/mazipan/bliblidotcom-chrome-extensions-unofficial',
+            'desc': 'Blibli.com Chrome Extensions'
+        }
+    ]
+}
